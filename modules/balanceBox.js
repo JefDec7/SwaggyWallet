788d79013fd161537adde8f3a144f68ee5577e33
Decorde Jeffrey <jeffrey.decorde@etu.utc.fr>
2016-11-07 10:38:22 +0100

    Fix on balance box

--- modules/balanceBox.js
+++ modules/balanceBox.js
@@ -5,7 +5,7 @@ export default function BalanceBox(props) {
   return <div id="balance-box" className="panel panel-default col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
     <div className="panel-body">
       <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <div>Current balance : </div>
+        <div>Balance : </div>
         {props.balance.toFixed(2)}{Constants.CURRENT_CURRENCY}
       </div>
       <div id="easter-egg" className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
